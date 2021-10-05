5e1d92ab3bd24425042da512ac9890fa1f816b11
Nicolas Maillat <maillat@alchemy.fr>
2021-10-05 18:34:26 +0200

    PHRAS-3505 bump php version

--- create-env-variable-doc.sh
+++ create-env-variable-doc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-php bundle_createEnvVariableDoc/createEnvVariableDoc.php
+php7.3 bundle_createEnvVariableDoc/createEnvVariableDoc.php
 
 # Uncomment php command line below if you want to test a local text file
 # See result in http://localhost:4041/en/Admin/EnvironnementVariables.html
