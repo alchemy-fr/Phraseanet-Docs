26c2ae2c8a5808eb3b18465c32192c5f5e0bf33c
felixalchemy <poimlk!>
2021-10-06 12:37:37 +0200

    PHRAS-3504: Specify php 7.3 version usage

--- create-env-variable-doc.sh
+++ create-env-variable-doc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-php bundle_createEnvVariableDoc/createEnvVariableDoc.php
+# note : php >= 7.1
+# Comment php command line below if you want to test a local text file
+php7.3 bundle_createEnvVariableDoc/createEnvVariableDoc.php
 
 # Uncomment php command line below if you want to test a local text file
 # See result in http://localhost:4041/en/Admin/EnvironnementVariables.html
-#php bundle_createEnvVariableDoc/createEnvVariableDoc.php bundle_createEnvVariableDoc/test/.envTest1.txt
+#php7.3 bundle_createEnvVariableDoc/createEnvVariableDoc.php bundle_createEnvVariableDoc/test/.envTest1.txt
